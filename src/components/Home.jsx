b44f679fc37849988bf8d7f4664436a82b534ecd
izaz ahmad <izazahmadhaider0@gmail.com>
2024-03-13 11:31:00 +0500

    .

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -16,7 +16,7 @@ function Home() {
       <Link to="/apireq" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
         Apireq
       </Link>
-      <Link to="/checksvg" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+      <Link to="/checksvg" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"> 
         Checksvg
       </Link>
     </div>
